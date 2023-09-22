bdc3b3539ddbd0ab8199d2975c89a6792415891b
nazem0_home_pc <mohamednazem09@gmail.com>
2023-09-23 01:26:22 +0300

    Added Animations

---
+++ assets/js/scrollreveal-implement.js
@@ -0,0 +1,13 @@
+import '../lib/scrollreveal.js'
+
+
+ScrollReveal({ distance: '100px' });
+ScrollReveal().reveal('nav', { origin: 'top', duration :1200 })
+ScrollReveal().reveal('.rightSide', { origin: 'right', duration :1200 })
+ScrollReveal().reveal('.leftSide', { origin: 'left', duration :1200 })
+ScrollReveal().reveal('.bottomSide', { origin: 'bottom', duration :1200 })
+ScrollReveal().reveal('.topSide', { origin: 'top', duration :1200 })
+
+ScrollReveal().reveal('.scaleView', { scale: 0.85, duration :1200 })
+
+
